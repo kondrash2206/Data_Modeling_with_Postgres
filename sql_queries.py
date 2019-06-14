7a547402812db6e1d57ed717d76c67e1254e0eef
kondrash2206 <48389160+kondrash2206@users.noreply.github.com>
2019-06-14 09:35:16 +0200

    commit after udacity rev1

--- sql_queries.py
+++ sql_queries.py
@@ -8,39 +8,64 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id numeric, start_time numeric, \
-                                                                   user_id int, level varchar, song_id varchar, \
-                                                                   artist_id varchar, session_id int, \
-                                                                   location varchar, user_agent varchar)""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, \
+                                                                  start_time numeric NOT NULL, \
+                                                                  user_id int NOT NULL, \
+                                                                  level varchar, \
+                                                                  song_id varchar, \
+                                                                  artist_id varchar, \
+                                                                  session_id int NOT NULL, \
+                                                                  location varchar, \
+                                                                  user_agent varchar)""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, \
-                                                              gender varchar, level varchar)""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, \
-                                                              year int, duration int)""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, \
+                                                        first_name varchar NOT NULL, \
+                                                          last_name varchar NOT NULL, \
+                                                          gender varchar NOT NULL, \
+                                                          level varchar NOT NULL)""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, \
-                                                               latitude numeric, longitude numeric)""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, \
+                                                          title varchar NOT NULL, \
+                                                          artist_id varchar NOT NULL, \
+                                                          year int NOT NULL, \
+                                                          duration int NOT NULL)""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time numeric, hour int, day varchar, week int, \
-                                                             month int, year int, weekday int)""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, \
+                                                              name varchar NOT NULL, \
+                                                              location varchar, \
+                                                              latitude numeric, \
+                                                              longitude numeric)""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time numeric PRIMARY KEY, \
+                                                         hour int NOT NULL, \
+                                                         day varchar NOT NULL, \
+                                                         week int NOT NULL, \
+                                                         month int NOT NULL, \
+                                                         year int NOT NULL, \
+                                                         weekday int NOT NULL)""")
+
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT into songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
-                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""")
+songplay_table_insert = ("""INSERT into songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""")
 
 user_table_insert = ("""INSERT into users (user_id, first_name, last_name, gender, level) \
-                        VALUES (%s,%s,%s,%s,%s)""")
+                        VALUES (%s,%s,%s,%s,%s) \
+                        ON CONFLICT (user_id) DO NOTHING""")
 
 song_table_insert = "INSERT into songs (song_id, title, artist_id, year, duration) \
-                        VALUES (%s, %s, %s, %s, %s)"
+                        VALUES (%s, %s, %s, %s, %s) \
+                        ON CONFLICT (song_id) DO NOTHING"
 
 artist_table_insert = ("""INSERT into artists (artist_id, name, location, latitude, longitude) \
-                         VALUES (%s,%s,%s,%s,%s)""")
+                         VALUES (%s,%s,%s,%s,%s) \
+                         ON CONFLICT (artist_id) DO NOTHING""")
 
 time_table_insert = ("""INSERT into time (start_time, hour, day, week, month, year, weekday) \
-                        VALUES (%s,%s,%s,%s,%s,%s,%s)""")
+                        VALUES (%s,%s,%s,%s,%s,%s,%s) \
+                        ON CONFLICT (start_time) DO NOTHING""")
 
 # FIND SONGS
 
